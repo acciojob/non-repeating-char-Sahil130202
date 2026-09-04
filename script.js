914324bcc5a607e5162072f59a4d7425ecf5e801
Sahil Nayak <sahilsony2002@gmail.com>
2026-09-04 14:21:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {}
+	for (let i = 0;i<str.length;i++){
+		if(count[str[i]]){
+			count[str[i]]++
+		}else{
+			count[str[i]]==1
+		}
+	}
+	for(let i=0;i<str.length;i++){
+		if(count[str[i]]===1){
+			return str[i]
+		}
+	}
+
+return null
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
